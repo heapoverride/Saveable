14d4c0cc844496231cd99ab543ff36b742d45182
UnrealSecurity <arran.bishop89@aol.com>
2023-01-25 18:59:07 +0200

    Initial commit with a built-in SaveableString class

---
+++ SaveableObject.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+
+namespace Saveable
+{
+    public abstract class SaveableObject
+    {
+        /// <summary>
+        /// Read <see cref="SaveableObject"/> from <see cref="BinaryReader"/>
+        /// </summary>
+        /// <param name="reader"></param>
+        public abstract void Read(BinaryReader reader);
+
+        /// <summary>
+        /// Write <see cref="SaveableObject"/> to <see cref="BinaryWriter"/>
+        /// </summary>
+        /// <param name="writer"></param>
+        public abstract void Write(BinaryWriter writer);
+
+        /// <summary>
+        /// Read <see cref="SaveableObject"/> from <see cref="BinaryReader"/>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="reader"></param>
+        /// <returns></returns>
+        public static T Read<T>(BinaryReader reader) where T : SaveableObject
+        {
+            var saveableObject = Activator.CreateInstance<T>();
+            saveableObject.Read(reader);
+            return saveableObject;
+        }
+
+        /// <summary>
+        /// Read array of <see cref="SaveableObject"/> from <see cref="BinaryReader"/>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="reader"></param>
+        /// <returns></returns>
+        public static T[] ReadArray<T>(BinaryReader reader) where T : SaveableObject
+        {
+            var array = new T[reader.ReadInt32()];
+
+            for (int i=0; i<array.Length; i++)
+            {
+                array[i] = Read<T>(reader);
+            }
+
+            return array;
+        }
+
+        /// <summary>
+        /// Write <see cref="SaveableObject"/> to <see cref="BinaryWriter"/>
+        /// </summary>
+        /// <param name="writer"></param>
+        /// <param name="saveableObject"></param>
+        public static void Write(BinaryWriter writer, SaveableObject saveableObject) => saveableObject.Write(writer);
+
+        /// <summary>
+        /// Write array of <see cref="SaveableObject"/> to <see cref="BinaryWriter"/>
+        /// </summary>
+        /// <param name="writer"></param>
+        /// <param name="array"></param>
+        public static void WriteArray(BinaryWriter writer, SaveableObject[] array)
+        {
+            writer.Write((int)array.Length);
+
+            foreach (var obj in array)
+            {
+                obj.Write(writer);
+            }
+        }
+    }
+}
---
+++ SaveableString.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System.Text;
+
+namespace Saveable
+{
+    public class SaveableString : SaveableObject
+    {
+        /// <summary>
+        /// Gets or sets the underlying <see cref="string"/> value
+        /// </summary>
+        public string Value { get; set; }
+
+        public SaveableString() { }
+
+        public SaveableString(string value)
+        {
+            Value = value;
+        }
+
+        /// <summary>
+        /// Implicit conversion from string to SaveableString
+        /// </summary>
+        /// <param name="value"></param>
+        public static implicit operator SaveableString(string value) => new SaveableString(value);
+
+        /// <summary>
+        /// Implicit conversion from SaveableString to string
+        /// </summary>
+        /// <param name="value"></param>
+        public static implicit operator string(SaveableString value) => value.Value;
+
+        public override void Read(BinaryReader reader)
+        {
+            Value = Encoding.UTF8.GetString(reader.ReadBytes(reader.ReadInt32()));
+        }
+
+        public override void Write(BinaryWriter writer)
+        {
+            writer.Write((int)Value.Length);
+            writer.Write(Encoding.UTF8.GetBytes(Value));
+        }
+
+        public override string ToString() => Value;
+    }
+}
