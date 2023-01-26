3af43492c78839787131ade97318e4e3f95bc217
UnrealSecurity <arran.bishop89@aol.com>
2023-01-26 12:19:37 +0200

    Added a summary to SaveableAttribute

--- SaveableAttribute.cs
+++ SaveableAttribute.cs
@@ -2,6 +2,9 @@
 
 namespace SaveableDotNet
 {
+    /// <summary>
+    /// Automatically read/write properties with Saveable attribute
+    /// </summary>
     [AttributeUsage(AttributeTargets.Property)]
     public class SaveableAttribute : Attribute
     {
